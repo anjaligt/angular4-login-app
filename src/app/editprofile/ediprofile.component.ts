3ffd513c785ce3d0450e4a08bde011f18508bf8c
VISCUS\anjalig <anjalig@vinfotech.com>
2018-03-09 17:46:04 +0530

    editprofile changes

--- src/app/editprofile/ediprofile.component.ts
+++ src/app/editprofile/ediprofile.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, Output, Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions } from "@angular/http";
-import { Router } from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
 import { LocalStorageService } from 'ngx-webstorage';
- 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {Md5} from 'ts-md5/dist/md5';
+
 @Component({
     templateUrl: './editprofile.component.html',
     styleUrls: ['./editprofile.component.css']
@@ -24,15 +26,20 @@ export class EditprofileComponent {
         this.session_data = this.session.retrieve('session_data');
     }
  
-    public register() {
+    updateProfile() {
+        alert('as');
+        console.log(this.input.first_name);
         if(this.input.email && this.input.password) {
+            var data = {};
             let headers = new Headers({ "content-type": "application/json" });
             let options = new RequestOptions({ headers: headers });
-            this.http.post("http://localhost:3000/account", JSON.stringify(this.input), options)
-                .map(result => result.json())
+            
+            //this.http.post("http://localhost:3000/account", JSON.stringify(this.input), options)
+              //  .map(result => result.json())
+              this.http.post("http://local.vsports.com/user/my_profile/update_profile", data)
                 .subscribe(result => {
                     this.router.navigate(["/login"]);
-                });
+               });
         }
     }
  
