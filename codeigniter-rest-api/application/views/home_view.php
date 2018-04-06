2c7b31ae03cf8702b6924f6dad0430dd970fa025
VISCUS\anjalig <anjalig@vinfotech.com>
2018-04-06 18:37:10 +0530

    add api folder

---
+++ codeigniter-rest-api/application/views/home_view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.0/css/font-awesome.css" />
+  <link rel="stylesheet" href="css/ngDialog-theme-default.css" />
+  <link rel="stylesheet" href="css/ngDialog.css" />
+  <!-- SPELLS -->
+  <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular.min.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular-route.js"></script>
+  <script src="/assets/script.js"></script>
+  <script src="js/ngDialog.js"></script>
+</head>
+<body>
+<h3>Home</h3>
+
+</body>
+</html>
